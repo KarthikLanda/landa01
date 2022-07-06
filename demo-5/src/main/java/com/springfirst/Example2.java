d443750d500312c73aa9f0fad9f52000df02ad44
Karthik <Karthik@DESKTOP-56LC309>
2022-07-06 12:02:33 +0530

    Added another class

---
+++ demo-5/src/main/java/com/springfirst/Example2.java
@@ -0,0 +1,11 @@
+package com.springfirst;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("kanaka raju");
+
+	}
+
+}
