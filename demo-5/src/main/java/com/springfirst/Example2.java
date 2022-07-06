dba83e8ef285325cb524bc2c23e9c3c88ef53345
Karthik <Karthik@DESKTOP-56LC309>
2022-07-06 14:51:57 +0530

    Changing name

--- demo-5/src/main/java/com/springfirst/Example2.java
+++ demo-5/src/main/java/com/springfirst/Example2.java
@@ -4,7 +4,7 @@ public class Example2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("kanaka raju");
+		System.out.println("geetika");
 
 	}
 
